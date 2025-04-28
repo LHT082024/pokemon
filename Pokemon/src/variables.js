dd1b2f975cc89a66d1bdee960ea349bc226c5e1a
LHT082024 <LHT082024@gmail.com>
2025-04-28 14:07:48 +0200

    calls for team

--- Pokemon/src/variables.js
+++ Pokemon/src/variables.js
@@ -1,4 +1,4 @@
-class trainer
+class Trainer
 {
     constructor(name, types, gender){
         this._name = name;
@@ -7,4 +7,16 @@ class trainer
     }
 }
 
-export default trainer;
+export default trainer; 
+
+class Team
+{
+    constructor(pk1, pk2, pk3, pk4, pk5, pk6){
+        this._pk1 = pk1;
+        this._pk2 = pk2;
+        this._pk3 = pk3;
+        this._pk4 = pk4;
+        this._pk5 = pk5;
+        this._pk6 = pk6;
+    }
+}
