6fdb586d6ffc631d69800dce09617c51e6f92988
LHT082024 <LHT082024@gmail.com>
2025-04-28 14:54:36 +0200

    images

--- Pokemon/src/Team.jsx
+++ Pokemon/src/Team.jsx
@@ -1,15 +1,16 @@
+import './App.css';
 import { Team } from "./variables";
 
 function DragonTeam()
 {
     return(
    <div className="teamDragon">
-    <img src="./src/assets/pokemon/Zekrom.png" alt="Zekrom" />
-    <img src="./src/assets/pokemon/Dragonair.png" alt="Zekrom" />
-    <img src="./src/assets/pokemon/Haxorus.png" alt="Zekrom" />
-    <img src="./src/assets/pokemon/Hydreigon.png" alt="Zekrom" />
-    <img src="./src/assets/pokemon/Kingdra.png" alt="Zekrom" />
-    <img src="./src/assets/pokemon/Salamence.png" alt="Zekrom" />
+    <img className='pokemon' src="./src/assets/pokemon/Zekrom.png" alt="Zekrom" />
+    <img className='pokemon' src="./src/assets/pokemon/Dragonair.png" alt="Zekrom" />
+    <img className='pokemon' src="./src/assets/pokemon/Haxorus.png" alt="Zekrom" />
+    <img className='pokemon' src="./src/assets/pokemon/Hydreigon.png" alt="Zekrom" />
+    <img className='pokemon' src="./src/assets/pokemon/Kingdra.png" alt="Zekrom" />
+    <img className='pokemon' src="./src/assets/pokemon/Salamence.png" alt="Zekrom" />
    </div> 
     )
 }
