8a69b00c12fc3d629cdaa6c4fe6cd376b25737be
LHT082024 <LHT082024@gmail.com>
2025-04-30 10:39:10 +0200

    fixed buttons

--- Pokemon/src/Team.jsx
+++ Pokemon/src/Team.jsx
@@ -8,19 +8,19 @@ function DragonTeam()
     <button className='pokemonButton'>
         <img className='pokemon' src="./src/assets/pokemon/Zekrom.png" alt="Zekrom" />
     </button>
-    <button>
+    <button className='pokemonButton'>
         <img className='pokemon' src="./src/assets/pokemon/Dragonair.png" alt="Zekrom" />
     </button>
-    <button>
+    <button className='pokemonButton'>
         <img className='pokemon' src="./src/assets/pokemon/Haxorus.png" alt="Zekrom" />
     </button>
-    <button>
+    <button className='pokemonButton'>
         <img className='pokemon' src="./src/assets/pokemon/Hydreigon.png" alt="Zekrom" />
     </button>
-    <button>
+    <button className='pokemonButton'>
         <img className='pokemon' src="./src/assets/pokemon/Kingdra.png" alt="Zekrom" />
     </button>
-    <button>
+    <button className='pokemonButton'>
         <img className='pokemon' src="./src/assets/pokemon/Salamence.png" alt="Zekrom" />
     </button>
    </div> 
