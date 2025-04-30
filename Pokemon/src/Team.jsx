94f0ee8dd79c540310f1a05ca9c62469ff54edf2
LHT082024 <LHT082024@gmail.com>
2025-04-30 10:28:26 +0200

    made the images into buttons

--- Pokemon/src/Team.jsx
+++ Pokemon/src/Team.jsx
@@ -5,12 +5,24 @@ function DragonTeam()
 {
     return(
    <div className="teamDragon">
-    <img className='pokemon' src="./src/assets/pokemon/Zekrom.png" alt="Zekrom" />
-    <img className='pokemon' src="./src/assets/pokemon/Dragonair.png" alt="Zekrom" />
-    <img className='pokemon' src="./src/assets/pokemon/Haxorus.png" alt="Zekrom" />
-    <img className='pokemon' src="./src/assets/pokemon/Hydreigon.png" alt="Zekrom" />
-    <img className='pokemon' src="./src/assets/pokemon/Kingdra.png" alt="Zekrom" />
-    <img className='pokemon' src="./src/assets/pokemon/Salamence.png" alt="Zekrom" />
+    <button onClick={"button clicked"}>
+        <img className='pokemon' src="./src/assets/pokemon/Zekrom.png" alt="Zekrom" />
+    </button>
+    <button>
+        <img className='pokemon' src="./src/assets/pokemon/Dragonair.png" alt="Zekrom" />
+    </button>
+    <button>
+        <img className='pokemon' src="./src/assets/pokemon/Haxorus.png" alt="Zekrom" />
+    </button>
+    <button>
+        <img className='pokemon' src="./src/assets/pokemon/Hydreigon.png" alt="Zekrom" />
+    </button>
+    <button>
+        <img className='pokemon' src="./src/assets/pokemon/Kingdra.png" alt="Zekrom" />
+    </button>
+    <button>
+        <img className='pokemon' src="./src/assets/pokemon/Salamence.png" alt="Zekrom" />
+    </button>
    </div> 
     )
 }
