e1adb2f0aff37b3185384ae2ed0729f6a4cb65d3
LHT082024 <LHT082024@gmail.com>
2025-05-05 14:29:59 +0200

    started pokemon array

--- Pokemon/src/variables.js
+++ Pokemon/src/variables.js
@@ -7,21 +7,21 @@ class Trainer
     }
 }
 
-class Teams
+class Pokemons
 {
-    constructor(pk1, pk2, pk3, pk4, pk5, pk6){
-        this._pk1 = pk1;
-        this._pk2 = pk2;
-        this._pk3 = pk3;
-        this._pk4 = pk4;
-        this._pk5 = pk5;
-        this._pk6 = pk6;
+    constructor(){
+        this.ImgPoke = [
+                "/src/assets/pokemon/Zekrom.png",
+
+            
+        ]
     }
 }
 
 
 //Array of trainer images
-class TrainerImages {
+class TrainerImages 
+{
     constructor() {
         this.imgsTrain = [
             "/src/assets/trainer/Blue.png",
