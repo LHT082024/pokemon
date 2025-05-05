0b10f1c7c841a6304e6b5929ce772c4a9e6c632c
LHT082024 <LHT082024@gmail.com>
2025-05-05 09:50:39 +0200

    added the pokemon components in app

--- Pokemon/src/App.jsx
+++ Pokemon/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import PokemonTrainer from "./pokemonStuff/PokemonTrainer";
+import { Team } from "./variables";
 
 function App() 
 {
