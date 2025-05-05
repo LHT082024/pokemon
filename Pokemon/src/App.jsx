c26618dd3f9d29d3aa9ba59d8ca0244cc0a30298
LHT082024 <LHT082024@gmail.com>
2025-05-05 13:55:56 +0200

    pokedex next to trainers

--- Pokemon/src/App.jsx
+++ Pokemon/src/App.jsx
@@ -12,32 +12,33 @@ function App()
   const trainer = new TrainerImages();
   const img = trainer.imgsTrain;
     return (
+
         //Trainer buttons
-    <div>
-        <nav id="trainerDisplay">
-         <TrainerButton imgSrc={img[0]} linkTo="/Team" />
-         <TrainerButton imgSrc={img[1]} linkTo="/Team" />
-         <TrainerButton imgSrc={img[2]} linkTo="/Team" />
-         <TrainerButton imgSrc={img[3]} linkTo="/Team" />
-        </nav>
+        <div className="trainerRows">
+            <div className="trainerDisplay">
+              <nav className="trainerRow">
+            <TrainerButton imgSrc={img[0]} linkTo="/Team" />
+            <TrainerButton imgSrc={img[1]} linkTo="/Team" />
+            <TrainerButton imgSrc={img[2]} linkTo="/Team" />
+            <TrainerButton imgSrc={img[3]} linkTo="/Team" />
+            </nav>
 
-         <nav id="trainerDisplay">
-         <TrainerButton imgSrc={img[4]} linkTo="/Team" />
-         <TrainerButton imgSrc={img[5]} linkTo="/Team" />
-         <TrainerButton imgSrc={img[6]} linkTo="/Team" />
-         <TrainerButton imgSrc={img[7]} linkTo="/Team" />
-        </nav>
+            <nav className="trainerDisplay">
+            <TrainerButton imgSrc={img[4]} linkTo="/Team" />
+            <TrainerButton imgSrc={img[5]} linkTo="/Team" />
+            <TrainerButton imgSrc={img[6]} linkTo="/Team" />
+            <TrainerButton imgSrc={img[7]} linkTo="/Team" />
+            </nav>
 
-        <div id="trainerDisplay">
-            <img src="src\assets\pokedex.png" alt="" />
+            <Routes>
+                <Route path="./pokemonStuff/Team" element={<DragonTeam />}/>
+            </Routes>
+
+            <div className="pokedexImage">
+            <img src="src/assets/pokedex.png" alt="Pokedex" />
+            </div>
+            </div>
         </div>
-
-        <Routes>
-            <Route path="./pokemonStuff/Team" element={<DragonTeam />}/>
-        </Routes>
-    </div>
-
-    
     );
 }
 
