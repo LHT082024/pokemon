01b825da3936a6dc3a3a907a20c93b7f8f0fe181
LHT082024 <LHT082024@gmail.com>
2025-05-06 10:38:08 +0200

    removed code not being used

--- Pokemon/src/App.jsx
+++ Pokemon/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 
 //Components
--- Pokemon/src/main.jsx
+++ Pokemon/src/main.jsx
@@ -1,7 +1,6 @@
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import TrainerButton from "./pokemonStuff/TrainerButton";
 import App from './App.jsx'
 import DragonTeam from './pokemonStuff/Team.jsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
