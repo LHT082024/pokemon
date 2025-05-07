ad328344b3d41f9d51a1113dc7ffbc628759fe95
LHT082024 <LHT082024@gmail.com>
2025-05-07 10:59:26 +0200

    single name from json

--- Pokemon/src/pokemonStuff/PokemonTrainer.jsx
+++ Pokemon/src/pokemonStuff/PokemonTrainer.jsx
@@ -3,15 +3,17 @@ import trainerData from "../assets/trainerModel.json";
 function PokemonTrainer() {
   console.log(trainerData)
   return(
+    // <div>
+    //   {trainerData.Trainers.map((el, index) => (
+    //     <div key={index}>
+    //       <p>{trainerData.Trainers[0].trainerName}</p>
+    //       {/* <img src={el.trainerImg} alt="" /> */}
+    //     </div>
+    //   ))}
+    // </div> 
     <div>
-      {trainerData.Trainers.map((el, index) => (
-        <div key={index}>
-          <p>{el.trainerName}</p>
-          <img src={el.trainerImg} alt="" />
-        </div>
-        
-      ))}
-    </div> 
+      {trainerData.Trainers[0].trainerName}
+    </div>
   );
   
 }
