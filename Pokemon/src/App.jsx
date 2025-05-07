df82d80136a141adecb7af8ab97c8cce7ae172a5
LHT082024 <LHT082024@gmail.com>
2025-05-07 11:36:45 +0200

    implimented trainer json into the buttons

--- Pokemon/src/App.jsx
+++ Pokemon/src/App.jsx
@@ -1,53 +1,47 @@
 import { Routes, Route, Link } from "react-router-dom";
 
 //Components
-import { TrainerImages } from "./variables";
 import DragonTeam from "./pokemonStuff/Team";
 import TrainerButton from "./pokemonStuff/TrainerButton";
 import PokemonTmodel from "./pokemonTrainers";
-import PokemonTrainer from "./pokemonStuff/PokemonTrainer";
 import './styling/masterCss.css'
+import trainerData from "./assets/trainerModel.json";
 
 function App() 
 {
-  // const trainerM = new PokemonTmodel();
-  // const trainer = new TrainerImages();
-  // const img = trainer.imgsTrain;
+  const trainerM = new PokemonTmodel();
+  const trainer = trainerData.Trainers
     return (
-      <PokemonTrainer />
 
-  //       //Trainer buttons with images and links
-  //       <div className="trainerRows">
-  //           <div className="trainerDisplay">
-  //             <nav className="trainerRow">
-  //               <h1>{trainerM.trainerName = "blue"}</h1>
+        //Trainer buttons with images and links
+        <div className="trainerRows">
+            <div className="trainerDisplay">
+              <nav className="trainerRow">
 
+            <TrainerButton id='blue' imgSrc={trainer[0].trainerImg} linkTo="/Team/blue" />
+            <TrainerButton id='calem'  imgSrc={trainer[1].trainerImg} linkTo="/Team/calem" />
+            <TrainerButton id='elio'  imgSrc={trainer[2].trainerImg} linkTo="/Team/elio" />
+            <TrainerButton id='gold' imgSrc={trainer[3].trainerImg} linkTo="/Team/gold" />
+            </nav>
 
+            <nav className="trainerDisplay">
+            <TrainerButton id='hilbert' imgSrc={trainer[4].trainerImg} linkTo="/Team/hilbert" />
+            <TrainerButton id='lucas' imgSrc={trainer[5].trainerImg} linkTo="/Team/lucas" />
+            <TrainerButton id='nate' imgSrc={trainer[6].trainerImg} linkTo="/Team/nate" />
+            <TrainerButton id='red' imgSrc={trainer[7].trainerImg} linkTo="/Team/red" />
+            </nav>
 
-  //           <TrainerButton id='blue' imgSrc={img[0]} linkTo="/Team/blue" />
-  //           <TrainerButton id='calem'  imgSrc={img[1]} linkTo="/Team/calem" />
-  //           <TrainerButton id='elio'  imgSrc={img[2]} linkTo="/Team/elio" />
-  //           <TrainerButton id='gold' imgSrc={img[3]} linkTo="/Team/gold" />
-  //           </nav>
+            <Routes>
+                <Route path="/Team/:id" element={<DragonTeam />}/>
+            </Routes>
+            </div>
 
-  //           <nav className="trainerDisplay">
-  //           <TrainerButton id='hilbert' imgSrc={img[4]} linkTo="/Team/hilbert" />
-  //           <TrainerButton id='lucas' imgSrc={img[5]} linkTo="/Team/lucas" />
-  //           <TrainerButton id='nate' imgSrc={img[6]} linkTo="/Team/nate" />
-  //           <TrainerButton id='red' imgSrc={img[7]} linkTo="/Team/red" />
-  //           </nav>
-
-  //           <Routes>
-  //               <Route path="/Team/:id" element={<DragonTeam />}/>
-  //           </Routes>
-  //           </div>
-
-  //           <div className="pokedexImage">
-  //             <button className="pokedexButton">
-  //                <img src="src/assets/pokedex.png" alt="Pokedex" />
-  //             </button>
-  //           </div>
-  //       </div>
+            <div className="pokedexImage">
+              <button className="pokedexButton">
+                 <img src="src/assets/pokedex.png" alt="Pokedex" />
+              </button>
+            </div>
+        </div>
     );
 }
 
--- Pokemon/src/pokemonStuff/PokemonTrainer.jsx
+++ Pokemon/src/pokemonStuff/PokemonTrainer.jsx
@@ -3,16 +3,9 @@ import trainerData from "../assets/trainerModel.json";
 function PokemonTrainer() {
   console.log(trainerData)
   return(
-    // <div>
-    //   {trainerData.Trainers.map((el, index) => (
-    //     <div key={index}>
-    //       <p>{trainerData.Trainers[0].trainerName}</p>
-    //       {/* <img src={el.trainerImg} alt="" /> */}
-    //     </div>
-    //   ))}
-    // </div> 
-    <div>
+    <div className="blueTrainerCard">
       {trainerData.Trainers[0].trainerName}
+      <img src={trainerData.Trainers[0].trainerImg} alt="blue" />
     </div>
   );
   
